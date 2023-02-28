9f4e53ce7d06a55b92ef1ed500105c558163dc67
amulayush <amulayush.ap@gmail.com>
2023-02-28 17:51:20 +0530

    Dashboard

--- smart-contact-manager/src/main/resources/static/js/script.js
+++ smart-contact-manager/src/main/resources/static/js/script.js
@@ -1,1 +1,21 @@
 console.log("this is script file")
+
+const toggleSideBar = () => {
+    if($(".sidebar").is(":visible")) { //we close it here
+        
+        $(".sidebar").css("display", "none");
+        $(".content").css("margin-left", "0%");
+
+    }
+
+    else { // we open it here
+
+        $(".sidebar").css("display", "block");
+        $(".content").css("margin-left", "20%");
+
+    }
+}
+
+console.log("this is script file")
+
+
